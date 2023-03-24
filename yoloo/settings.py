df938bb504942d5e25da79fce9f26d37fdcba0c5
Utkarsh Kumar <utkarshk384@gmail.com>
2023-03-24 05:53:15 +0530

    Updated .env location

--- yoloo/settings.py
+++ yoloo/settings.py
@@ -14,13 +14,12 @@ from pathlib import Path
 from datetime import timedelta
 import os, environ
 
-env = environ.Env()
-environ.Env.read_env()
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
