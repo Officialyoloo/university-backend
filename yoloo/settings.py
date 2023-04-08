500e63eace7fc187c390afd4a87b5543ee671349
Officialyoloo <101660162+Officialyoloo@users.noreply.github.com>
2023-04-09 04:09:38 +0530

    Update settings.py

--- yoloo/settings.py
+++ yoloo/settings.py
@@ -142,7 +142,7 @@ USE_TZ = True
 # Serve static files from the static folder
 STATIC_ROOT =os.path.join(BASE_DIR, 'image-data')
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
